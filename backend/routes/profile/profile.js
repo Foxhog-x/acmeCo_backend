91a68baa87385f8fc388501cd227efd0a09b06d0
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-06-19 01:15:39 +0530

    deployement ready

--- backend/routes/profile/profile.js
+++ backend/routes/profile/profile.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
-router.get("/", (req, res) => {
-  console.log(req.body);
+router.post("/", (req, res) => {
+  try {
+    const formData = req.body;
+    let jsonData = [];
+    try {
+      jsonData = JSON.parse(fs.readFileSync("data.json"));
+    } catch (error) {
+      console.error("Error reading file:", error);
+    }
+
+    jsonData.push(formData);
+
+    fs.writeFile("data.json", JSON.stringify(jsonData, null, 2), (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        res.status(500).send("Error writing file");
+        return;
+      }
+      console.log("Data appended to file successfully");
+      res.send("Data appended to file successfully");
+    });
+    res.status(200).json({ success: formData });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
 });
 
 module.exports = router;
