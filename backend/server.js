941c159060bd70eb90710630d59d7baeb26f84e7
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-06-19 01:15:39 +0530

    all working

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
+const path = require("path");
+const fs = require("fs");
+const cors = require("cors");
+const multer = require("multer");
+const uploadDir = path.join(__dirname, "./uploads");
 
-app.use("/profile", require("./routes/profile/profile"));
+app.use(cors());
 
-app.listen(8000, () => {
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.use(
+  "/profile",
+  upload.single("image"),
+  require("./routes/profile/profile")
+);
+
+app.listen(PORT, () => {
   console.log(` port is running on ${PORT}`);
 });
