8233106c0ca81e449fe8aa1bc44aa5e0b373120c
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-06-19 15:48:16 +0530

    fix

--- backend/routes/profile/profile.js
+++ backend/routes/profile/profile.js
@@ -1,39 +1,31 @@
-const fs = require("fs");
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 
 router.post("/", (req, res) => {
   try {
     const formData = req.body;
     let jsonData = [];
-
-    // Check if the file exists before reading it
-    if (fs.existsSync("data.json")) {
-      try {
-        const fileData = fs.readFileSync("data.json", "utf8");
-        jsonData = JSON.parse(fileData);
-        if (!Array.isArray(jsonData)) {
-          throw new Error("File does not contain an array");
-        }
-      } catch (error) {
-        console.error("Error parsing file:", error);
-        return res.status(500).send("Error parsing file");
-      }
+    try {
+      jsonData = JSON.parse(fs.readFileSync("data.json"));
+    } catch (error) {
+      console.error("Error reading file:", error);
     }
+
     jsonData.push(formData);
 
     fs.writeFile("data.json", JSON.stringify(jsonData, null, 2), (err) => {
       if (err) {
         console.error("Error writing file:", err);
-        return res.status(500).send("Error writing file");
+        res.status(500).send("Error writing file");
+        return;
       }
-
-      console.log("Form data saved successfully:", formData);
-      return res.status(200).json({ success: formData });
+      console.log("Data appended to file successfully");
+      res.send("Data appended to file successfully");
     });
+    res.status(200).json({ success: formData });
   } catch (error) {
-    console.error("Error:", error);
-    return res.status(400).json({ error: error.message });
+    res.status(400).json(error.message);
   }
 });
 
