0bf710b4669f701974395efb50197e0c5d190de5
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-06-19 15:58:42 +0530

    added file limit

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,12 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+});
 app.get("/", (req, res) => {
   res.send("Server api is running successfully ");
 });
